a1341d1330903db5c4089a5552f09eb487e58314
Jamie Fraser <jamiedf8@gmail.com>
2019-05-04 11:05:35 +0100

    Update json.js

--- json.js
+++ json.js
@@ -4,8 +4,8 @@ $().ready(function(){
     $.getJSON( "/log.json", function( data ) {
     console.log(data);
 
-      var timeData;
-      var moistureData;
+      var timeData = [];
+      var moistureData = [];
 
       for (i = 0; i < data.length; i++) { 
         timeData.push(data[i]['time'])
